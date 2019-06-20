e5d23828c1a9fb43c85d24c80998cb1039006f30
Erick Bran <27997erick@gmail.com>
2019-06-20 13:40:07 -0600

    primera subida

---
+++ src/models/conferencia.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var CharlaSchema = Schema({
+    nombreCharla:String,
+	descripcion: String,
+    comunicador: String,
+    salon: String,
+    numeroAsiento:Number,
+    hora:Date,
+    fecha: Date,
+    capacidad: Number,
+    ocupados: [{type: Schema.ObjectId, ref: 'User'}]
+});
+
+module.exports = mongoose.model('Conferencia', CharlaSchema);
